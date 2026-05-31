2e2961aa7ad4874e7832dd9571afa64ba0661182
Ubuntu <ubuntu@ubuhtu-hermes.anbob1jaikcu1luqzai1a3xr1c.cx.internal.cloudapp.net>
2026-05-31 20:38:06 +0000

    feat: add GitHub Actions CI workflow

--- src/application/ports/todo-repository.ts
+++ src/application/ports/todo-repository.ts
@@ -1,10 +1,10 @@
-import type { CreateTodoDto, UpdateTodoDto } from '../dto/todo-dto.ts';
-import type { Todo } from '../../domain/entities/todo.ts';
+import type { CreateTodoDto, UpdateTodoDto } from "../dto/todo-dto.ts";
+import type { Todo } from "../../domain/entities/todo.ts";
 
-export interface TodoRepository {
-  findAll(): Promise<Todo[]>;
-  findById(id: string): Promise<Todo | undefined>;
-  create(input: CreateTodoDto): Promise<Todo>;
-  update(id: string, input: UpdateTodoDto): Promise<Todo>;
-  delete(id: string): Promise<void>;
+export abstract class TodoRepository {
+  abstract findAll(): Promise<Todo[]>;
+  abstract findById(id: string): Promise<Todo | undefined>;
+  abstract create(input: CreateTodoDto): Promise<Todo>;
+  abstract update(id: string, input: UpdateTodoDto): Promise<Todo>;
+  abstract delete(id: string): Promise<void>;
 }
--- src/application/use-cases/todo-use-cases.ts
+++ src/application/use-cases/todo-use-cases.ts
@@ -1,52 +1,57 @@
-import type { CreateTodoDto, TodoResponse, UpdateTodoDto } from '../dto/todo-dto.ts';
-import type { Todo } from '../../domain/entities/todo.ts';
-import type { TodoRepository } from '../ports/todo-repository.ts';
-import { NotFoundError } from '../../domain/errors/domain-error.ts';
+import { Box } from "@catniplabs/box";
+import type { CreateTodoDto, UpdateTodoDto } from "../../application/dto/todo-dto.ts";
+import type { Todo } from "../../domain/entities/todo.ts";
+import { NotFoundError } from "../../domain/errors/domain-error.ts";
+import { DenoKvTodoRepository } from "../../infra/db/deno-kv-todo-repository.ts";
 
+@Box.Service({ deps: [DenoKvTodoRepository] })
 export class CreateTodoUseCase {
-  public constructor(private readonly todoRepository: TodoRepository) {}
+  public constructor(private readonly todoRepository: DenoKvTodoRepository) {}
 
-  public async execute(input: CreateTodoDto): Promise<TodoResponse> {
-    const todo = await this.todoRepository.create(input);
-    return todo;
+  public async execute(input: CreateTodoDto): Promise<Todo> {
+    return await this.todoRepository.create(input);
   }
 }
 
+@Box.Service({ deps: [DenoKvTodoRepository] })
 export class GetAllTodosUseCase {
-  public constructor(private readonly todoRepository: TodoRepository) {}
+  public constructor(private readonly todoRepository: DenoKvTodoRepository) {}
 
-  public async execute(): Promise<TodoResponse[]> {
+  public async execute(): Promise<Todo[]> {
     return await this.todoRepository.findAll();
   }
 }
 
+@Box.Service({ deps: [DenoKvTodoRepository] })
 export class GetTodoByIdUseCase {
-  public constructor(private readonly todoRepository: TodoRepository) {}
+  public constructor(private readonly todoRepository: DenoKvTodoRepository) {}
 
-  public async execute(id: string): Promise<TodoResponse> {
+  public async execute(id: string): Promise<Todo> {
     const todo = await this.todoRepository.findById(id);
     if (!todo) {
-      throw new NotFoundError('Todo', id);
+      throw new NotFoundError("Todo", id);
     }
     return todo;
   }
 }
 
+@Box.Service({ deps: [DenoKvTodoRepository] })
 export class UpdateTodoUseCase {
-  public constructor(private readonly todoRepository: TodoRepository) {}
+  public constructor(private readonly todoRepository: DenoKvTodoRepository) {}
 
-  public async execute(id: string, input: UpdateTodoDto): Promise<TodoResponse> {
+  public async execute(id: string, input: UpdateTodoDto): Promise<Todo> {
     return await this.todoRepository.update(id, input);
   }
 }
 
+@Box.Service({ deps: [DenoKvTodoRepository] })
 export class DeleteTodoUseCase {
-  public constructor(private readonly todoRepository: TodoRepository) {}
+  public constructor(private readonly todoRepository: DenoKvTodoRepository) {}
 
   public async execute(id: string): Promise<void> {
     const todo = await this.todoRepository.findById(id);
     if (!todo) {
-      throw new NotFoundError('Todo', id);
+      throw new NotFoundError("Todo", id);
     }
     await this.todoRepository.delete(id);
   }
